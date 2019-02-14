775761ae55b61bc10e35d2d641d6f0de37f1d525
Tania.Pash <t.pashnyuk@gmail.com>
2019-02-14 11:56:09 +0700

    fix: rm ml module

--- cc/cvTypes/imgprocConstants.h
+++ cc/cvTypes/imgprocConstants.h
@@ -1,7 +1,7 @@
 #include "macros.h"
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
-#include <opencv2/ml.hpp>
+// #include <opencv2/ml.hpp>
 
 #ifndef __FF_IMGPROCCONSTANTS_H__
 #define __FF_IMGPROCCONSTANTS_H__
